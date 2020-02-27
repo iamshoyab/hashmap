67a56fe497751c888a1d26231468165da64dd725
shoyab8382 <48435228+shoyab8382@users.noreply.github.com>
2020-02-27 10:41:49 +0530

    Add files via upload

---
+++ code (1).java
@@ -0,0 +1,15 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		HashMap<Integer,String> hash_map=new HashMap<Integer,String>();
+		hash_map.put(1,"Red");
+		hash_map.put(2,"Green");
+		hash_map.put(3,"Blue");
+		hash_map.put(4,"Yellow");
+		System.out.println(hash_map.size());
+	}
+}
